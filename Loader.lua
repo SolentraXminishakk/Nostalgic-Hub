d01fbef15d2720e5df8b49443214f963997fc1e0
Solentra <soulxz2007@gmail.com>
2025-09-15 20:18:18 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 -- this is for multi game support (dont touch unless you wanna make an extension to the games for your own proj)
 
 if game.PlaceId == 142823291 then
-  loadstring
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/MainScript.lua"))()
 end
