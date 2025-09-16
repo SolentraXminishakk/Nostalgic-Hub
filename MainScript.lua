89bf2c74de6e12733e0766fc55de4aa3c83ec735
Solentra <soulxz2007@gmail.com>
2025-09-15 20:17:54 -0500

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,3 @@
+-- heres the script src (complicated so I wouldnt touch around this)
+local Libary = loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/library.lua"))()
+local Players = game:GetService("Players")
