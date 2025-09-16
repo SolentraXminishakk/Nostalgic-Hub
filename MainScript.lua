ec61d313214c3d5b3f112ca861758e7cc81e9b23
Solentra <soulxz2007@gmail.com>
2025-09-15 20:29:15 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,6 @@
 -- heres the script src (complicated so I wouldnt touch around this)
 local Libary = loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/library.lua"))()
+local silentCheck = loadstring(game:HttpGet(""))()
 local Players = game:GetService("Players")
 local RS = game:GetService("RunService")
 local WS = game:GetService("Workspace")
