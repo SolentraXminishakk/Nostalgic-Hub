820094ebd02c0ab7cbfc6ee2dd6e6b1791665358
Solentra <soulxz2007@gmail.com>
2025-09-16 18:42:03 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,2 @@
+-- NOTE THIS WILL GRAB YOUR HWID SO THAT WE WILL BE ABLE TO BLACKLIST YOU. PLEASE JOIN DISCORD.GG/TEAMNOSTALGIC FOR MORE INFO !! BY RUNNING THIS YOU WILL CONSENT!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/Loader.lua"))()
