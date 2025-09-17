20a8f763d589a4d2fe46e5778a06361262116bea
Solentra <soulxz2007@gmail.com>
2025-09-16 19:31:59 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,13 +1,6 @@
 -- heres the script src (complicated so I wouldnt touch around this)
 local Libary = loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/library.lua"))()
 local silentCheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/Nostalgic-Hub/refs/heads/main/silentCheck.lua"))()
-local Players = game:GetService("Players")
-local RS = game:GetService("RunService")
-local WS = game:GetService("Workspace")
-local Player = Players.LocalPlayer
-local Char = Player.Character or Player.CharacterAdded:Wait() -- safe load
-local Humanoid = Char.Humanoid
-local HRP = Char.HumanoidRootPart
 
 -- Tabs
 local VisualTab = Library:CreateTab({
