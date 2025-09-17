3ba978bba60dea14cde34f353bd4a3ce5434ed9e
Solentra <soulxz2007@gmail.com>
2025-09-16 19:35:51 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -7,4 +7,19 @@ local VisualTab = Library:CreateTab({
   Name = "Visuals"    
 })
 
+local PlayerTab = Library:CreateTab({
+  Name = "Player"    
+})
+
+local GameModsTab = Library:CreateTab({
+  Name = "Game Mods"    
+})
+
+local TeleportTab = Library:CreateTab({
+  Name = "Teleport"    
+})
+
+local FreemotesTab = Library:CreateTab({
+  Name = "Free Emotes"    
+})
 Check()
